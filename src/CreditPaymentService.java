ae3170cb89ca5d48b8a92d4aab2011903205048f
Daria Burdakova <d.saharova@inbox.ru>
2022-05-31 17:16:19 +0300

    Error fixed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public double calculate(int credit, double rate, int months) {
 
-        double monthlyRate = rate / (100 * months);
+        double monthlyRate = rate / (100 * 12);
         double payment = credit * (monthlyRate / (1 - Math.pow((1 + monthlyRate), -months)));
         return payment;
     }
